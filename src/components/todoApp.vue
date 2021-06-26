9f73ee98f00245b8f7cffa3669bbcc9811d3e004
MichalMiekina <47924236+MichalMiekina@users.noreply.github.com>
2021-06-26 12:55:21 +0200

    Add readme file

---
+++ src/components/todoApp.vue
@@ -0,0 +1,121 @@
+<template>
+  <div id="nav">
+    <input type="text" placeholder="Wpisz zadanie..." v-model="newTask" />
+    <button @click="handleButton">Dodaj</button>
+
+    <section>
+      <h3>Lista zadań</h3>
+
+      <div
+        class="task"
+        v-for="task in tasks"
+        v-bind:key="task.id"
+        v-bind:class="{
+          completed: task.completed,
+        }"
+        @click="checkTask(task.id)"
+      >
+        <span>{{ task.name }}</span>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      tasks: [{ name: 'zrobić kupę', completed: false, id: 'df2fed' }],
+    };
+  },
+  methods: {
+    handleButton() {
+      if (this.newTask.length > 0) {
+        this.tasks.push({
+          name: this.newTask,
+          completed: false,
+          id: Math.random(),
+        });
+        this.newTask = '';
+      }
+    },
+    checkTask(id) {
+      const index = this.tasks.findIndex((el) => el.id === id);
+      this.tasks[index].completed = true;
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+
+}
+
+*,
+*::after,
+*::before {
+  box-sizing: border-box;
+}
+
+html,
+body {
+  margin: 0;
+  padding: 0;
+}
+
+#app {
+  max-width: 500px;
+  padding: 30px 0;
+  margin: 0 auto;
+  background-color: rgb(255, 150, 150);
+}
+
+h1 {
+  text-align: center;
+}
+
+main {
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+}
+
+section {
+  flex-basis: 100%;
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  text-align: center;
+}
+
+section h3 {
+  flex-basis: 100%;
+}
+.task {
+  flex-basis: 100%;
+  display: flex;
+  justify-content: center;
+}
+.task span {
+  width: 250px;
+  padding: 20px 0;
+  border: 1px solid red;
+  margin-top: 10px;
+  background-color: rgb(255, 100, 100);
+}
+
+.completed span {
+  background-color: gray;
+  text-decoration: line-through;
+}
+</style>
